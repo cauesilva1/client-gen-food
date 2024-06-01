f1c37633c4debdbe4385b119904b98beeeb81626
cauesilva1 <cauecatonesilva@gmail.com>
2024-05-31 22:20:53 -0400

    feat:  criar pagina de load 5

--- src/pages/Load.jsx
+++ src/pages/Load.jsx
@@ -2,31 +2,30 @@ import React from "react";
 import { useState, useEffect } from "react";
 import "../stylePages/Load.css";
 import axios from "axios";
-import { Link } from "react-router-dom";
 
 export default function Load() {
   const [url, setUrl] = useState("");
 
-  useEffect(() => {
-    const fullUrl = window.location.href;
-    setUrl(fullUrl);    
+//   useEffect(() => {
+//     const fullUrl = window.location.href;
+//     setUrl(fullUrl);    
     
-    const token = fullUrl.split("#")[1];
+//     const token = fullUrl.split("#")[1];
 
-    const EnviarJwt = async (event) => {
-        try {
-          const response = await axios.post(
-            "https://gen-food.vercel.app/updateUser",
-            token
-          );
-          console.log("Data submitted successfully:", response.data);
-        } catch (error) {
-          console.error("There was an error submitting the data!", error);
-        }
-      };
+//     const EnviarJwt = async (event) => {
+//         try {
+//           const response = await axios.post(
+//             "https://gen-food.vercel.app/updateUser",
+//             token
+//           );
+//           console.log("Data submitted successfully:", response.data);
+//         } catch (error) {
+//           console.error("There was an error submitting the data!", error);
+//         }
+//       };
 
-    EnviarJwt();
-  }, []);
+//     EnviarJwt();
+//   }, []);
 
 
   async function Logar() {
@@ -40,10 +39,9 @@ export default function Load() {
     }
   }
    
-
   return <div className="loadText">
     <h1>Loading...</h1>
 
-    <button onClick={Logar()}>Logar</button>
+    <button onClick={Logar}>Logar</button>
     </div>;
 }
