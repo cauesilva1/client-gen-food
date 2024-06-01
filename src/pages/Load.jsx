48255901cf44827ca55b3d9b918fcd8d5c80c72c
cauesilva1 <cauecatonesilva@gmail.com>
2024-05-31 22:42:25 -0400

    feat:  criar pagina de load 5

--- src/pages/Load.jsx
+++ src/pages/Load.jsx
@@ -6,46 +6,32 @@ import axios from "axios";
 export default function Load() {
   const [url, setUrl] = useState("");
 
-//   useEffect(() => {
-//     const fullUrl = window.location.href;
-//     setUrl(fullUrl);    
+  useEffect(() => {
+    const fullUrl = window.location.href;
+    setUrl(fullUrl);    
     
-//     const token = fullUrl.split("#")[1];
+    const token = fullUrl.split("#")[1];
 
-//     const EnviarJwt = async (event) => {
-//         try {
-//           const response = await axios.post(
-//             "https://gen-food.vercel.app/updateUser",
-//             token
-//           );
-//           console.log("Data submitted successfully:", response.data);
-//         } catch (error) {
-//           console.error("There was an error submitting the data!", error);
-//         }
-//       };
-
-//     EnviarJwt();
-//   }, []);
-
-
-  async function Logar() {
-    try {
-      const response = await axios.get(
-        "https://gen-food.vercel.app/login",
-        Headers = {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
+    const EnviarJwt = async (event) => {
+        try {
+          const response = await axios.post(
+            "https://gen-food.vercel.app/updateUser",
+            token
+          );
+          console.log("Data submitted successfully:", response.data);
+        } catch (error) {
+          console.error("There was an error submitting the data!", error);
         }
-      );
-      console.log("Data submitted successfully:", response.data);
-    } catch (error) {
-      console.error("There was an error submitting the data!", error);
-    }
-  }
+      };
+
+    EnviarJwt();
+  }, []);
+
    
   return <div className="loadText">
     <h1>Loading...</h1>
 
-    <button onClick={Logar}>Logar</button>
+    <a href="https://gen-food.vercel.app/login">Logar</a>
+
     </div>;
 }
